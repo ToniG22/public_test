cf2751501ff01df9baef91f14825027442fbd50e
ToniG22 <tonigarces19@gmail.com>
2022-11-15 16:29:57 +0000

    IDK

--- main.c
+++ main.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
-{
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
+{   
+    //Coment
     printf("Hello, World!\n");
     return 0;
 }
